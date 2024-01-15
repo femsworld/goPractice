e1d1b99be6d449736da27271e7c19af8b1eb24c2
femsworld <femsworld@gmail.com>
2024-01-15 22:22:12 +0200

    Edited6

--- count.sh
+++ count.sh
@@ -1,9 +1,4 @@
-# count=$(find . -type f | wc -l)
-# result=$((count * 5))
-# printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
-
-count=$(ls -1 | wc -l)
+# count=$(ls -1 | wc -l)
+count=$(ls . | wc -l)
 result=$((count * 5))
-#  printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
-# printf "\t^KTotal files * %d: %d^K\n" "$count" "$result" | sed -e 's/\t/^I/g'
 printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
