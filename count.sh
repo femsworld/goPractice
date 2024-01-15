1a2b5c990775c7475c951d36fa38fae7f23df507
femsworld <femsworld@gmail.com>
2024-01-15 21:24:05 +0200

    Edited5

--- count.sh
+++ count.sh
@@ -1,3 +1,7 @@
-count=$(find . -type f | wc -l)
+# count=$(find . -type f | wc -l)
+# result=$((count * 5))
+# printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
+
+count=$(ls -1 | wc -l)
 result=$((count * 5))
-printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
+ printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
