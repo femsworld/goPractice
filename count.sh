932b7d906eaac003e4dff5a72fdcf77b5a3508a1
femsworld <femsworld@gmail.com>
2024-01-15 21:29:12 +0200

    Edited6

--- count.sh
+++ count.sh
@@ -4,4 +4,6 @@
 
 count=$(ls -1 | wc -l)
 result=$((count * 5))
- printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
+#  printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
+# printf "\t^KTotal files * %d: %d^K\n" "$count" "$result" | sed -e 's/\t/^I/g'
+printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
