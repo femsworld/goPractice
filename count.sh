cb701b35fc9130c4b252cb53ffa1d239aafde500
femsworld <femsworld@gmail.com>
2024-01-15 22:40:20 +0200

    Edited8

--- count.sh
+++ count.sh
@@ -2,4 +2,5 @@
 count=$(ls . | wc -l)
 result=$((count * 5))
 # printf "\t^KTotal files * %d: %d^K\n" "$count" "$result"
-printf "\\t\\vTotal files * %d: %d\\v\\n" "$count" "$result"
+# printf "\\t\\vTotal files * %d: %d\\v\\n" "$count" "$result"
+printf "\\t\\v^KTotal files * %d: %d^K\\v\\n" "$count" "$result"
