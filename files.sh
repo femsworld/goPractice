bd4fd3f01ef8994232d59bf586be4157c4b6808b
femsworld <femsworld@gmail.com>
2024-01-16 00:13:19 +0200

    Edited6

--- files.sh
+++ files.sh
@@ -8,10 +8,16 @@ fi
 
 cp '!' '\`/'
 
-# if [ "$MOVE_A" == "yes" ]; then
-#     mv a '\`/'
-# fi
-mv a '\`/'
+if [ "$MOVE_A" == "yes" ]; then
+    # Move the file a into the  directory
+    mv a ''
+elif [ "$MOVE_A" == "no" ]; then
+    # Remove the file a
+    rm a
+else
+    # Leave the file a as is
+    :
+fi
 
 # Remove file 'a' if MOVE_A is set to 'no'
 if [ "$MOVE_A" == "no" ]; then
