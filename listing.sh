39e606d924d754254e314a1c45d70f4e63c1ccc4
femsworld <femsworld@gmail.com>
2024-01-15 17:13:42 +0200

    Edited

--- listing.sh
+++ listing.sh
@@ -1,5 +1,5 @@
 ls -la 
 ls --ignore={.,..}
-ls --format=comma --group-directories-first --quoting-style=shell
-ls -lt
+ls --quoting-style=shell | awk '{for (i=1; i<=NF; i++) printf "%s%s", $i, (i<NF ? ", " : RS)}'
+ls -ltu
 ls -lF --indicator-style=slash
