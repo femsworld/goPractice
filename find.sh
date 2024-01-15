6d9b3595fabea2635511779d7aed3078b0e6ce03
femsworld <femsworld@gmail.com>
2024-01-16 01:31:31 +0200

    Edited

--- find.sh
+++ find.sh
@@ -1,1 +1,9 @@
-find . -type f \( -name "a*" -o -name "*z" -o -name "z*a" \)
+#!/bin/bash
+
+# Look for files starting with 'a'
+find . -type f -name 'a*' 
+
+find . -type f -name '*z'
+
+# Look for files starting with 'z' and ending with 'a'
+find . -type f -name 'z*a'
