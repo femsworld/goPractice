c4ad2b83a5505dc435e28440f77bb0520184c421
femsworld <femsworld@gmail.com>
2024-01-15 21:56:16 +0200

    Edited2

--- files.sh
+++ files.sh
@@ -3,19 +3,19 @@
 # Create files a, !, \, and "
 touch a '!' '\' '"'
 
-# Create a directory called `
-mkdir '\`'
+# Create a directory called ` if it doesn't exist
+if [ ! -d '\`' ]; then
+    mkdir '\`'
+fi
 
 # Copy the file '!' into the ` directory
 cp '!' '\`/'
 
 # Check the environment variable
-if [ "$MY_VARIABLE" == "yes" ]; then
+if [ "$MOVE_A" == "yes" ]; then
+    # If MOVE_A is set to 'yes', move file 'a' into the ` directory
     mv a '\`/'
-elif [ "$MY_VARIABLE" == "no" ]; then
-    # If MY_VARIABLE is set to 'no', remove file 'a'
+elif [ "$MOVE_A" == "no" ]; then
+    # If MOVE_A is set to 'no', remove file 'a'
     rm a
-else
-    # Otherwise, leave the file 'a' as is
-    :
 fi
