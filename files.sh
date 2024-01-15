d152335c66153f9ddc3470995cc46d74b9c4588f
femsworld <femsworld@gmail.com>
2024-01-15 22:11:25 +0200

    Edited6

--- files.sh
+++ files.sh
@@ -1,47 +1,16 @@
-# #!/bin/bash
-
-# # Create files a, !, \, and "
-# touch a '!' '\' '"'
-
-# # Create a directory called ` if it doesn't exist
-# if [ ! -d '\`' ]; then
-#     mkdir '\`'
-# fi
-
-# # Copy the file '!' into the ` directory
-# cp '!' '\`/'
-
-# # Check the environment variable
-# if [ "$MOVE_A" == "yes" ]; then
-#     # If MOVE_A is set to 'yes', move file 'a' into the ` directory
-#     mv a '\`/'
-# elif [ "$MOVE_A" == "no" ]; then
-#     # If MOVE_A is set to 'no', remove file 'a'
-#     rm a
-# fi
-
 #!/bin/bash
 
-# Create files a, !, \, and "
 touch a '!' '\' '"'
 
-# Create a directory called ` if it doesn't exist
 if [ ! -d '\`' ]; then
     mkdir '\`'
 fi
 
-# Copy the file '!' into the ` directory
 cp '!' '\`/'
 
-# Check the environment variable
 if [ "$MOVE_A" == "yes" ]; then
-    # If MOVE_A is set to 'yes', move file 'a' into the ` directory
     mv a '\`/'
 fi
-
-# Print the final state
-echo "Final state:"
-ls -R
 
 # Remove file 'a' if MOVE_A is set to 'no'
 if [ "$MOVE_A" == "no" ]; then
