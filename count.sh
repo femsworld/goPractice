db72b4ff8971454aadf584c0ad8cc1fd5db72ee8
femsworld <femsworld@gmail.com>
2024-01-15 20:36:06 +0200

    Edited

--- count.sh
+++ count.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-count=$(find . -type f -or -type d -exec printf x \; | wc -c)
+# count=$(find . -type f -or -type d -exec printf x \; | wc -c)
+# result=$((count * 5))
+
+# printf "\tTotal files * 5: %d\n" "$result"
+
+count=$(find . -type f | wc -l)
 result=$((count * 5))
-
-printf "\tTotal files * 5: %d\n" "$result"
+# printf "$result"
+printf "\tTotal file * %d: %d\n" "$count" "$result"
