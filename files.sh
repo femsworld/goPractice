16371d8bb141338a18c7ec0af13544eceec975fb
femsworld <femsworld@gmail.com>
2024-01-16 00:07:11 +0200

    Edited5

--- files.sh
+++ files.sh
@@ -8,9 +8,10 @@ fi
 
 cp '!' '\`/'
 
-if [ "$MOVE_A" == "yes" ]; then
-    mv a '\`/'
-fi
+# if [ "$MOVE_A" == "yes" ]; then
+#     mv a '\`/'
+# fi
+mv a '\`/'
 
 # Remove file 'a' if MOVE_A is set to 'no'
 if [ "$MOVE_A" == "no" ]; then
