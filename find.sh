76a59fd1098f4ca1588e7e99fbb7b1111cecd46e
femsworld <femsworld@gmail.com>
2024-01-16 01:49:56 +0200

    Edited4

--- find.sh
+++ find.sh
@@ -7,11 +7,17 @@
 
 # find . -type f -name 'z*a'
 
+
 #!/bin/bash
 
-# Look for files starting with 'a'
-find . -type f -name 'a*' -exec echo {} \;
+# Find and display files starting with 'a'
+echo "Files starting with 'a':"
+find . -type f -name 'a*' 
 
-find . -type f -name '*z' -exec echo {} \;
+# Find and display files ending with 'z'
+echo -e "\nFiles ending with 'z':"
+find . -type f -name '*z'
 
-find . -type f -name 'z*a' -exec echo {} \;
+# Find and display files starting with 'z' and ending with 'a'
+echo -e "\nFiles starting with 'z' and ending with 'a':"
+find . -type f -name 'z*a'
