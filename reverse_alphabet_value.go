56bf025c0640749bf8b7bbed7bcf5bd46aaa306f
femsworld <femsworld@gmail.com>
2024-01-16 12:31:00 +0200

    convert to lowercaseRune

--- reverse_alphabet_value.go
+++ reverse_alphabet_value.go
@@ -3,6 +3,9 @@ package sprint
 func ReverseAlphabetValue(ch rune) rune {
 	if ch >= 'a' && ch <= 'z' {
 		return 'z' - (ch - 'a')
+	} else if ch >= 'A' && ch <= 'Z' { 
+		lowercaseRune := ch + ('a' - 'A')
+		return 'z' - (lowercaseRune - 'a')
 	} else {
 		return ch
 	}
