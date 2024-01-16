8fb850e74226c30e3cc4001d057c355418e43a30
femsworld <femsworld@gmail.com>
2024-01-16 02:01:45 +0200

    Edited7

--- find.sh
+++ find.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Look for files starting with 'a'
-# find . -type f -name 'a*' 
-
-# find . -name 'a*'
-
-# find . -type f -name '*z'
-
-# find . -type f -name 'z*a'
-
 find . \( -name 'a*' -o -type f -name '*z' -o -type f -name 'z*a' \)
 
 
