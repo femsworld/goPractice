205438d4ef023572664033fced8b891da7afcd02
femsworld <femsworld@gmail.com>
2024-01-16 11:27:40 +0200

    Edited4

--- mean.go
+++ mean.go
@@ -4,3 +4,24 @@ func Mean(a, b, c float32) float32 {
 	result := (a + + c)/3.0
 	return result
 }
+
+
+
+
+
+// func Mean(numbers ...float32) float32 {
+//     sum := float32(0)
+//     count := len(numbers)
+
+//     for _, num := range numbers {
+//         sum += num
+//     }
+
+//     if count > 0 {
+//         mean := sum / float32(count)
+//         return mean
+//     }
+
+//     return 0
+// }
+
