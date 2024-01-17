6a9b4bceb3b30807f19da9024cea5cc88a8e4077
femsworld <femsworld@gmail.com>
2024-01-17 14:05:39 +0200

    Added count_divisible.go

--- is_leap_year.go
+++ is_leap_year.go
@@ -2,11 +2,11 @@ package sprint
 
 func IsLeapYear(year int) bool {
 	if year%4 == 0 {
-        if year%100 == 0 {
-            return year%400 == 0
-        }
-        return true
-    }
-    return false
+		if year%100 == 0 {
+			return year%400 == 0
+		}
+		return true
+	}
+	return false
 
 }
