2b67a4170aef8c01cc19a65d850ebfa5b6b5c4a1
femsworld <femsworld@gmail.com>
2024-01-18 14:45:23 +0200

    Edited

--- pairs.go
+++ pairs.go
@@ -7,13 +7,11 @@ func Pairs() string {
 
 	for i := 10; i < 100; i++ {
 		for j := i + 1; j < 100; j++ {
-			// result += fmt.Sprintf("%02d %02d, ", i, j)
 			result += fmt.Sprintf("%02d %02d", i, j)
 			if i != 98 || j != 99 {
 				result += ", "
 			}
 		}
 	}
-	// Remove the trailing ", " before returning the result
 	return result
 }
