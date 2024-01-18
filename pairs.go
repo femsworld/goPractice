6b3f9cd5dd1a77dc9a1b3b4f7f4e4bacddaa0173
femsworld <femsworld@gmail.com>
2024-01-18 14:48:23 +0200

    Edited

--- pairs.go
+++ pairs.go
@@ -5,7 +5,7 @@ import "fmt"
 func Pairs() string {
 	result := ""
 
-	for i := 10; i < 100; i++ {
+	for i := 0; i < 100; i++ {
 		for j := i + 1; j < 100; j++ {
 			result += fmt.Sprintf("%02d %02d", i, j)
 			if i != 98 || j != 99 {
