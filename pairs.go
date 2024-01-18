97b26a556f55975e8869bec2481aace3bcdd56fd
femsworld <femsworld@gmail.com>
2024-01-18 14:40:05 +0200

    Edited

--- pairs.go
+++ pairs.go
@@ -5,13 +5,15 @@ import "fmt"
 func Pairs() string {
 	result := ""
 
-	for i := 10; i <= 99; i++ {
-		for j := i + 1; j <= 99; j++ {
+	for i := 10; i < 100; i++ {
+		for j := i + 1; j < 100; j++ {
 			// result += fmt.Sprintf("%02d %02d, ", i, j)
-			result += fmt.Sprintf("%02d %02d, ", i, j)
+			result += fmt.Sprintf("%02d %02d", i, j)
+			if i != 98 || j != 99 {
+				result += ", "
+			}
 		}
 	}
 	// Remove the trailing ", " before returning the result
-	return result[:len(result)-2]
-
+	return result
 }
