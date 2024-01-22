54520a98f6d1b0b7da26e137b2a02955480c3d46
femsworld <femsworld@gmail.com>
2024-01-22 15:48:42 +0200

    Added FilterBySum

--- filter_by_sum.go
+++ filter_by_sum.go
@@ -4,23 +4,19 @@ func FilterBySum(arr [][]int, limit int) [][]int {
 	var result [][]int
 
 	for _, subarray := range arr {
-		sum := calculateSum(subarray)
+		sum := 0
+		for _, value := range subarray {
+			sum += value
+		}
+
 		if sum >= limit {
 			result = append(result, subarray)
 		}
 	}
 
 	if len(result) == 0 {
-		return nil
+		return [][]int{}
 	}
 
 	return result
-}
-
-func calculateSum(subarray []int) int {
-	sum := 0
-	for _, value := range subarray {
-		sum += value
-	}
-	return sum
 }
