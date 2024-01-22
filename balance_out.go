f450aa39d2ba1cb74ed8e6c49ad4da1c7c57b9ba
femsworld <femsworld@gmail.com>
2024-01-22 14:38:48 +0200

    Edited

--- balance_out.go
+++ balance_out.go
@@ -1,8 +1,24 @@
 package sprint
 
 func BalanceOut(arr []bool) []bool {
-	// Count the number of true and false values in the array
+	trueCount, falseCount := countBooleans(arr)
+
+	for trueCount != falseCount {
+		if trueCount < falseCount {
+			arr = append(arr, true)
+			trueCount++
+		} else {
+			arr = append(arr, false)
+			falseCount++
+		}
+	}
+
+	return arr
+}
+
+func countBooleans(arr []bool) (int, int) {
 	trueCount, falseCount := 0, 0
+
 	for _, value := range arr {
 		if value {
 			trueCount++
@@ -10,20 +26,5 @@ func BalanceOut(arr []bool) []bool {
 			falseCount++
 		}
 	}
-	// Calculate the number of booleans needed to balance the array
-	minimumToAdd := abs(trueCount - falseCount)
-
-	// Add the minimum number of booleans necessary
-	for i := 0; i < minimumToAdd; i++ {
-		arr = append(arr, !arr[0]) // add the opposite of the first element
-	}
-
-	return arr
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+	return trueCount, falseCount
 }
