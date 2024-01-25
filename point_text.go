5644f5fc8945d0edc625dd08d83d8fc85370819f
femsworld <femsworld@gmail.com>
2024-01-25 13:07:14 +0200

    Edited

--- point_text.go
+++ point_text.go
@@ -3,6 +3,6 @@ package sprint
 import "fmt"
 
 func PointText(p Point) Point {
-	formattedText := fmt.Sprintf("%s at (%f, %f)", p.Text, p.X, p.Y)
-	return MakePoint(p.X, p.Y, formattedText)
+	formattedText := fmt.Sprintf("Text at (%f, %f)", p.X, p.Y)
+	return Point{X: p.X, Y: p.Y, Text: formattedText}
 }
