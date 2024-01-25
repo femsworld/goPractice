24bc7a60d691863480be2dc30546eff5ad17a128
femsworld <femsworld@gmail.com>
2024-01-25 13:09:46 +0200

    Added interface

--- point_text.go
+++ point_text.go
@@ -2,6 +2,12 @@ package sprint
 
 import "fmt"
 
+type Point struct {
+	X    float32
+	Y    float32
+	Text string
+}
+
 func PointText(p Point) Point {
 	formattedText := fmt.Sprintf("Text at (%f, %f)", p.X, p.Y)
 	return Point{X: p.X, Y: p.Y, Text: formattedText}
