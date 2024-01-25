1435c0455721afbd3dd5e6b6fcd130a232ecc3f9
femsworld <femsworld@gmail.com>
2024-01-25 12:17:42 +0200

    Added PointDiff

---
+++ point_diff.go
@@ -0,0 +1,20 @@
+package sprint
+
+import "math"
+
+type Coordinates struct {
+	X float32
+	Y float32
+	Text string
+}
+
+func PointDiff(p1, p2 Point) Point {
+	distance1 := math.Sqrt(float64(p1.X*p1.X + p1.Y*p1.Y))
+	distance2 := math.Sqrt(float64(p2.X*p2.X + p2.Y*p2.Y))
+
+	if distance1 >= distance2 {
+		return p1
+	}
+	return p2
+}
+
