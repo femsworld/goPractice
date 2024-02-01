9615356c878005e949a5167ebbd34d926a26a072
femsworld <femsworld@gmail.com>
2024-02-01 23:33:11 +0200

    Edited

--- payout.go
+++ payout.go
@@ -1,9 +1,17 @@
 package sprint
 
+import "sort"
+
 func Payout(amount int, denominations []int) []int {
+	if amount < 0 || len(denominations) == 0 || !isPositiveDenominations(denominations) {
+		return []int{} // Invalid input cases
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(denominations))) // Sort denominations in descending order
+
 	result := []int{}
 
-	for i := len(denominations) - 1; i >= 0; i-- {
+	for i := 0; i < len(denominations); i++ {
 		for amount >= denominations[i] {
 			result = append(result, denominations[i])
 			amount -= denominations[i]
@@ -14,5 +22,14 @@ func Payout(amount int, denominations []int) []int {
 		return result
 	}
 
-	return []int{}
+	return []int{} // Return an empty array if the payout cannot be made
+}
+
+func isPositiveDenominations(denominations []int) bool {
+	for _, denom := range denominations {
+		if denom <= 0 {
+			return false
+		}
+	}
+	return true
 }
