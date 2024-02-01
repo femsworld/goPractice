b55a015f200639a106f479d3bdaaf8c42170b313
femsworld <femsworld@gmail.com>
2024-02-01 22:30:40 +0200

    Edited

--- l_c_m.go
+++ l_c_m.go
@@ -1,14 +1,14 @@
 package sprint
 
 func GCD1(a, b int) int {
-    for b != 0 {
-        a, b = b, a%b
-    }
-    return a
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
 
 func LCM(a, b int) int {
-    gcd := GCD1(a, b)
-    lcm := (a * b) / gcd
-    return lcm
+	gcd := GCD1(a, b)
+	lcm := (a * b) / gcd
+	return lcm
 }
--- sort_word_arr.go
+++ sort_word_arr.go
@@ -1,8 +1,18 @@
 package sprint
 
-import "sort"
+func Swap(a []string, i, j int) {
+	temp := a[i]
+	a[i] = a[j]
+	a[j] = temp
+}
 
 func SortWordArr(a []string) []string {
-	sort.Strings(a)
+	for i := 0; i < len(a)-1; i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[i] > a[j] {
+				Swap(a, i, j)
+			}
+		}
+	}
 	return a
 }
