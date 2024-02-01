5d4f6abdcfdf0fdb2acada0b2f4344fb41f62f20
femsworld <femsworld@gmail.com>
2024-02-01 21:47:30 +0200

    Edited

--- is_palindrome.go
+++ is_palindrome.go
@@ -3,13 +3,14 @@ package sprint
 import "strings"
 
 func IsPalindrome(s string) bool {
-	s = strings.ToLower(strings.ReplaceAll(s, " ", " "))
+	s = strings.ToLower(strings.ReplaceAll(s, " ", ""))
 
+	// Compare the string with its reverse
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		if s[i] != s[j] {
 			return false
 		}
 	}
-	return true
 
+	return true
 }
