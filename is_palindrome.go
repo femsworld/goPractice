b5cfad52557e756bf76f8a2ea07431f5ed505371
femsworld <femsworld@gmail.com>
2024-02-01 21:44:22 +0200

    Added IsPalindrome

---
+++ is_palindrome.go
@@ -0,0 +1,15 @@
+package sprint
+
+import "strings"
+
+func IsPalindrome(s string) bool {
+	s = strings.ToLower(strings.ReplaceAll(s, " ", " "))
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+
+}
